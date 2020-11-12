377ebe93aab8ca3ab1cbaccb1b4c4598bd1c62b6
Koladele Olaitan <koladeleolaitan@gmail.com>
2020-11-12 14:38:00 +0100

    created navbar, added more responsiveness for mobile

---
+++ nav.js
@@ -0,0 +1,28 @@
+const navSlide = () => {
+    const burger = document.querySelector('.burger');
+    const nav = document.querySelector('.nav');
+    const navLinks = document.querySelectorAll('.nav-links li');
+
+    
+    burger.addEventListener('click',()=>{
+        //toggle nav
+       nav.classList.toggle('nav-active');  
+
+        //animate links
+        navLinks.forEach((link, index)=>{
+            if(link.style.animation){
+                link.style.animation = ''
+            } else {
+                link.style.animation = `navLinkFade 0.5s ease forwards ${index / 5 + 0.4}s`;
+            }
+        })
+
+        //burger toggle
+        burger.classList.toggle('toggle');
+    });
+
+
+
+}
+
+navSlide();
